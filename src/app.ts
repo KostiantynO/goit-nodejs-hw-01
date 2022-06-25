ca5639c713636baf78c2b9f56da7a9e2ff1d75b0
Kostiantyn Ochenash <Ochenashk@gmail.com>
2022-06-25 22:51:14 +0300

    adds screenshots links to readme

--- src/app.ts
+++ src/app.ts
@@ -13,30 +13,4 @@ program.parse(process.argv);
 const argv = program.opts();
 console.log({ argv });
 
-(async () => {
-  await invokeAction(argv);
-
-  // const contacts = await listContacts();
-  // console.log({ contacts });
-
-  // const newContact = await addContact({
-  //   name: 'Mango',
-  //   email: 'mango@mail.com',
-  //   phone: '0980700700',
-  // });
-  // console.log({ newContact });
-  const id = 'e686e00f-3a28-41fb-b443-9c2474ab29d6';
-  // const contact = await getContactById(id);
-  // console.log({ contact });
-
-  // const updatedContact = await editContact({
-  //   id,
-  //   name: 'Kiwi',
-  //   email: 'kiwi@mail.com',
-  //   phone: '0980123456',
-  // });
-  // console.log({ updatedContact });
-
-  // const removedContact = await removeContact(id);
-  // console.log({ removedContact });
-})();
+(async () => await invokeAction(argv))();
